128c40ba0bacb7183e2f5448da9c2aa8dfa1c7a6
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-07 23:22:54 +0530

    Create ForLoop.java

---
+++ Control_Constructs/LoopingStatements/ForLoop.java
@@ -0,0 +1,19 @@
+package control_constructs;
+//FOR LOOOP
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int j=0;
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+			System.out.println(j);
+		}
+		
+			int jj=5;
+			for(int ii=0;ii<jj;ii++) {
+				System.out.println(ii);
+				System.out.println(jj);
+	
+		}
+	}
+}
