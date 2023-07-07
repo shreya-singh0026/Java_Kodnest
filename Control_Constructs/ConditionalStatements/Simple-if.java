444e3951a85be1b64ac5a596c004954f19a3122e
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-07 23:19:27 +0530

    Create Simple-if.java

---
+++ Control_Constructs/ConditionalStatements/Simple-if.java
@@ -0,0 +1,15 @@
+package control_constructs;
+import java.util.*;
+public class If_statement {
+//IF STATEMENT
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int marks = sc.nextInt();
+		sc.nextLine();
+ 		if(marks>90)
+			System.out.println("Welcome to TechClub");
+	
+	}
+
+}
