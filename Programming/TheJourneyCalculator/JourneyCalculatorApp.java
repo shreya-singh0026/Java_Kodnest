3aa236812f5490dca3b29c7a8cf167eb07b6b789
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-09 03:37:53 +0530

    Create JourneyCalculatorApp.java

---
+++ Programming/TheJourneyCalculator/JourneyCalculatorApp.java
@@ -0,0 +1,10 @@
+
+public class JourneyCalculatorApp {
+
+	public static void main(String[] args) {
+		JourneyCalculator jc = new JourneyCalculator();
+		System.out.printf("%.2f", jc.calculateDistance(60.0, 1.5));
+	}
+
+}
+
