1dd74429789648fd74123930f4edae6e707d6be7
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-08 22:13:23 +0530

    Create HalveNumberApp.java

---
+++ Programming/HalveIt/HalveNumberApp.java
@@ -0,0 +1,12 @@
+
+public class HalveNumberApp {
+	public static void main(String[] args) {
+		System.out.println(halveTheNumber(150.00));
+	}
+
+	public static double halveTheNumber(double num) {
+		return (num/2);
+	}
+
+}
+	
