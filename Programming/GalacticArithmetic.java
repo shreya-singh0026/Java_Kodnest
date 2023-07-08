5115a68581e10b32e57cd016f63be8588b1f9d64
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-08 09:14:25 +0530

    Update GalacticArithmetic.java

--- Programming/GalacticArithmetic.java
+++ Programming/GalacticArithmetic.java
@@ -1,3 +1,5 @@
+//Problem : Galactic Arithmetic
+
 /*In a far-off galaxy named Arithmetica, beings have been using a newly discovered number system to make their life easier.
  * z However, they are facing difficulties in performing basic arithmetic operations. As the resident genius of planet Earth,
  *  you have been called upon to help them. Your first task is to build a function that will be able to add two numbers in their number system.
