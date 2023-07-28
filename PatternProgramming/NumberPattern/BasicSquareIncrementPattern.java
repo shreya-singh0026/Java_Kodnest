0006c225ac0694eef0d85c2982d4ae7daa8d48db
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 14:09:24 +0530

    Add files via upload

---
+++ PatternProgramming/NumberPattern/BasicSquareIncrementPattern.java
@@ -0,0 +1,26 @@
+/*
+ * 
+1111
+2222
+3333
+4444
+
+*/
+
+
+package com.kodnest.numberpattern;
+
+public class BasicSquareIncrementPattern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<5;i++) {
+			for(int j=1;j<5;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+
+		}
+	}
+
+}
