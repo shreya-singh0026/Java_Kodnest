adb823521d0d42203556fdb1b09582a74256d065
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 11:56:15 +0530

    Create CreateAnArray.java

---
+++ ArrayProgrammings/CreateAnArray.java
@@ -0,0 +1,29 @@
+/*
+1)Write a java Program to create an integer array by taking length as input from the user .
+Take the contents of the array from user and store the contents in the array. 
+Display the contents of the array on to the console
+ */
+
+package com.kodnest.arrays.level1;
+
+import java.util.Scanner;
+
+public class CreateAnIntegerArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Array Size : ");
+		int arr[]=new int[sc.nextInt()];
+		System.out.print("Enter Array Integer : ");
+
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.print("Print Array Integer : ");
+
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+ " ");
+		}
+	
+	}
+}
