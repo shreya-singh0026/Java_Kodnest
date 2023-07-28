e90a20b57439d3679c9e52e22cb53c9c1c3a10e1
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 09:57:22 +0530

    Add files via upload

---
+++ PatternProgramming/RectanglePattern.java
@@ -0,0 +1,24 @@
+/*
+  *****
+  *****
+  *****
+  *****
+  *****
+ */
+
+
+package com.kodnest.patterns.level4;
+
+public class RectanglePattern {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
