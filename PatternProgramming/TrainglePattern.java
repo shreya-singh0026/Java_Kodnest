71bc7256a30c9d5e9b80f6080d59f23e0e43e246
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 10:58:26 +0530

    Add files via upload

---
+++ PatternProgramming/TrainglePattern.java
@@ -0,0 +1,28 @@
+/*
+
+*
+**
+***
+****
+*****
+******
+
+ */
+
+
+package com.kodnest.patterns.level6;
+
+public class TrainglePattern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {		
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+
+	}
+	}
+}
+	
