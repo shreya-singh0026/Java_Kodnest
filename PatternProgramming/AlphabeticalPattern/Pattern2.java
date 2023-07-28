df2476ba5efa906a03d8db342470206d4a56648b
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 10:05:26 +0530

    Add files via upload

---
+++ PatternProgramming/AlphabeticalPattern/Pattern2.java
@@ -0,0 +1,30 @@
+/*
+ 
+A
+AB
+ABC
+ABCD
+ABCDE
+
+ */
+
+
+package com.kodnest.patterns.level5;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		char ch='A';
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(ch);
+				ch++;
+			}
+			ch='A';
+			System.out.println();
+	    }
+
+	}
+	
+
+}
