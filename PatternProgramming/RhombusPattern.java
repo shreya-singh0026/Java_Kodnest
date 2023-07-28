594f6ecabcea94e57c6692b74f8a4e024b132614
Shreya Singh <85695858+shreya-singh0026@users.noreply.github.com>
2023-07-28 10:48:33 +0530

    Add files via upload

---
+++ PatternProgramming/RhombusPattern.java
@@ -0,0 +1,29 @@
+/*
+
+ ****
+  ****
+   ****
+    ****
+ 
+ */
+
+package com.kodnest.patterns.level4;
+
+public class RhombusPattern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print(" ");
+			}		
+			for(int j=1;j<=6;j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+
+		
+	
+	}
+	}
+}
